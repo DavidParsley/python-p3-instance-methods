8f27b97750e1f933bd3d2b606fe26e215d1694a7
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-12-01 17:59:17 +0300

    adds completed lab

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,15 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print( "Hello World!")
+    def walk(self):
+        print("The person is walking.")    
+
+David = Person()
+David.talk()
+David.walk()
+
+Parsley=Person()
+Parsley.talk()
+Parsley.walk()
